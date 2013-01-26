cdce1489dbf2851c92a38d49b0055b665c55a5e0
kyokutyo <haruta@kyokutyo.com>
2013-01-26 18:15:33 +0900

    Set parameters out of while loop

--- chapter06/ex02.rb
+++ chapter06/ex02.rb
@@ -2,11 +2,14 @@
 
 puts "耳の遠いおばあちゃんのプログラム"
 
+year = min_year = 1930
+rand_max = 21
+
 message = ""
 while message != 'BYE'
   message = gets.chomp
   if message == message.upcase && message != 'BYE'
-    year = 1930 + rand(21)
+    year = min_year + rand(rand_max)
     puts "いやー、#{year}年以来ないねー!"
   else
     puts "は?! もっと大きな声で話しておくれ、坊や!"
--- chapter06/ex03.rb
+++ chapter06/ex03.rb
@@ -4,12 +4,15 @@ puts "耳の遠いおばあちゃんのプログラム(拡張)"
 
 # 三回連続でBYEと叫ばないと繰り返し抜けられない
 
+year = min_year = 1930
+rand_max = 21
+
 messages = ['']
 while messages != ['BYE', 'BYE', 'BYE']
   message = gets.chomp
   if message == 'BYE'
   elsif message == message.upcase
-    year = 1930 + rand(21)
+    year = min_year + rand(rand_max)
     puts "いやー、#{year}年以来ないねー!"
   else
     puts "は?! もっと大きな声で話しておくれ、坊や!"
