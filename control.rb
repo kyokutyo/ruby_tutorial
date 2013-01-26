e83dc0989a7f88b0f2156f7b765547dba59c4aae
kyokutyo <haruta@kyokutyo.com>
2013-01-26 18:01:05 +0900

    Create ex02.rb and remove moved lines

--- control.rb
+++ control.rb
@@ -125,45 +125,6 @@ puts (not i_am_Chris)
 
 puts "====================="
 
-puts "耳の遠いおばあちゃんのプログラム"
-
-message = ""
-while message != 'BYE'
-  message = gets.chomp
-  if message == message.upcase && message != 'BYE'
-    year = 1930 + rand(21)
-    puts "いやー、#{year}年以来ないねー!"
-  else
-    puts "は?! もっと大きな声で話しておくれ、坊や!"
-  end
-end
-
-puts "耳の遠いおばあちゃんのプログラムおわり"
-
-puts "====================="
-
-puts "耳の遠いおばあちゃんのプログラム(拡張)"
-
-# 三回連続でBYEと叫ばないと繰り返し抜けられない
-
-messages = ['']
-while messages != ['BYE', 'BYE', 'BYE']
-  message = gets.chomp
-  if message == 'BYE'
-  elsif message == message.upcase
-    year = 1930 + rand(21)
-    puts "いやー、#{year}年以来ないねー!"
-  else
-    puts "は?! もっと大きな声で話しておくれ、坊や!"
-  end
-  messages.unshift(message)
-  messages = messages.slice(0,3)
-end
-
-puts "耳の遠いおばあちゃんのプログラム(拡張)おわり"
-
-puts "====================="
-
 puts "うるう年"
 
 # 4で割り切れる年はうるう年, しかし100で割り切れる年はうるう年ではない, しかし400で割り切れる年はうるう年
