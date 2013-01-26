d4088bcd169609b31ed8aab11785a30cdacb51a9
kyokutyo <haruta@kyokutyo.com>
2013-01-26 17:48:44 +0900

    Use downto

--- chapter06/ex01.rb
+++ chapter06/ex01.rb
@@ -10,13 +10,12 @@ def get_numeration(num)
   end
 end
 
-99.times do |i| # 0..99
-  num = 99 - i
-  numeration = get_numeration(num)
-  s  = "#{num} #{numeration} of beer on the wall\n"
-  s += "#{num} #{numeration} of beer\n"
+99.downto(1) do |i|
+  numeration = get_numeration(i)
+  s  = "#{i} #{numeration} of beer on the wall\n"
+  s += "#{i} #{numeration} of beer\n"
   s += "Take one down and pass it around\n"
-  numeration = get_numeration(num - 1)
-  s += "#{num - 1} #{numeration} of beer on the wall\n\n"
+  numeration = get_numeration(i - 1)
+  s += "#{i - 1} #{numeration} of beer on the wall\n\n"
   puts s
 end
